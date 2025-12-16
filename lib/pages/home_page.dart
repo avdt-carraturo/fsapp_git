27d31866ec8f8cb21556767648092d9c5fec6ef4
Antonio Carraturo <a.carraturo@almaviva.it>
2025-12-16 14:31:42 +0100

    Fix 16/12/2025

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:fsapp_shared/shared.dart';
 import 'package:fsapp/widgets/confirm_button.dart';
 import 'package:fsapp/pages/notifications_page.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'dart:io';
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   final String? serial;
@@ -65,6 +70,7 @@ class _HomePageState extends State<HomePage> {
     return ok;
   }
 
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -110,8 +116,10 @@ class _HomePageState extends State<HomePage> {
 
                   //Bottone 1
                   _AlertButton(
-                    title: 'Emergenza Silenziosa',
-                    subtitle: 'Aggressione o minaccia',
+                    //title: 'Emergenza Silenziosa',
+                    title: 'Aggressione',
+                    //subtitle: 'Aggressione o minaccia',
+                    subtitle: 'Segnala una minaccia verbale o fisica',
                     icon: Icons.warning_amber,
                     color: Colors.red,
                     serial: treno,
@@ -123,7 +131,8 @@ class _HomePageState extends State<HomePage> {
                   //Bottone 2
                   _AlertButton(
                     title: 'Emergenza Medica',
-                    subtitle: 'Malore o pronto soccorso',
+                    //subtitle: 'Malore o pronto soccorso',
+                    subtitle: 'Avvia una chiamata al 112',
                     icon: Icons.favorite,
                     color: Colors.pink,
                     serial: treno,
@@ -134,8 +143,10 @@ class _HomePageState extends State<HomePage> {
 
                   //Bottone 3
                   _AlertButton(
-                    title: 'Molestia Personale',
-                    subtitle: 'Comportamenti inappropriati',
+                    //title: 'Molestia Personale',
+                    title: 'Molestia',
+                    //subtitle: 'Comportamenti inappropriati',
+                    subtitle: 'Segnala una molestia vissuta o della quale sei testimone',
                     icon: Icons.people,
                     color: Colors.orange,
                     serial: treno,
@@ -155,6 +166,18 @@ class _HomePageState extends State<HomePage> {
                     validator: _canOpen,
                     currentUser: widget.currentUser,
                   ),
+
+                  //Bottone 5
+                  _AlertButton(
+                    title: 'Comportamento intemperante',
+                    subtitle: 'Segnala la presenza di una persona in stato psicofisico alterato',
+                    icon: Icons.people_outlined,
+                    color: Colors.grey,
+                    serial: treno,
+                    carriage: carrozza,
+                    validator: _canOpen,
+                    currentUser: widget.currentUser,
+                  ),
                 ],
               ),
             ),
@@ -165,7 +188,7 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
-class _AlertButton extends StatelessWidget {
+class _AlertButton extends StatefulWidget {
   final String title;
   final String subtitle;
   final IconData icon;
@@ -183,8 +206,31 @@ class _AlertButton extends StatelessWidget {
     required this.serial,
     required this.carriage,
     required this.validator,
-    required this.currentUser
+    required this.currentUser,
   });
+
+  @override
+  State<_AlertButton> createState() => _AlertButtonState();
+}
+
+class _AlertButtonState extends State<_AlertButton> {
+  late TextEditingController _trenoCtrl;
+  late TextEditingController _carrozzaCtrl;
+  XFile? mediaFile;
+
+  @override
+  void initState() {
+    super.initState();
+    _trenoCtrl = TextEditingController(text: widget.serial ?? '');
+    _carrozzaCtrl = TextEditingController(text: widget.carriage ?? '');
+  }
+
+  @override
+  void dispose() {
+    _trenoCtrl.dispose();
+    _carrozzaCtrl.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -193,95 +239,245 @@ class _AlertButton extends StatelessWidget {
       margin: const EdgeInsets.symmetric(vertical: 8),
       child: ListTile(
         leading: CircleAvatar(
-            backgroundColor: color, child: Icon(icon, color: Colors.white)),
-        title: Text(title, style: const TextStyle(fontWeight: FontWeight.bold)),
-        subtitle: Text(subtitle),
+          backgroundColor: widget.color,
+          child: Icon(widget.icon, color: Colors.white),
+        ),
+        title: Text(widget.title, style: const TextStyle(fontWeight: FontWeight.bold)),
+        subtitle: Text(widget.subtitle),
         trailing: const Icon(Icons.chevron_right),
-        onTap: () {
-          if (!validator(title)) return;
-          
+        onTap: () => _handleTap(context),
+      ),
+    );
+  }
 
-          showDialog(
-            context: context,
-            builder: (_) => AlertDialog(
-              title: Text("Conferma segnalazione"),
-              content: Text(
-                (serial != null && serial!.isNotEmpty)
-                    ? "Stai aprendo una segnalazione $title per:\n\n"
-                        "• Treno: $serial\n"
-                        "• Carrozza: $carriage\n\n"
-                        "Sei sicuro?\nL'apertura di una segnalazione fasulla è un crimine."
-                    : "Aprire la segnalazione?",
+  void _handleTap(BuildContext context) {
+    if (!widget.validator(widget.title)) return;
+
+    if (widget.title == 'Emergenza Medica') {
+      _showMedicalDialog(context);
+    } else {
+      _showConfirmDialog(context);
+    }
+  }
+
+  Future<void> _pickMedia() async {
+    final picker = ImagePicker();
+    final file = await picker.pickImage(source: ImageSource.camera);
+    if (file != null) {
+      setState(() => mediaFile = file);
+    }
+  }
+
+  void _showMedicalDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        title: const Text("Emergenza Medica"),
+        content: RichText(
+          text: TextSpan(
+            style: const TextStyle(color: Colors.black, fontSize: 16),
+            children: [
+              const TextSpan(text: "Puoi chiamare il "),
+              const TextSpan(
+                text: "112",
+                style: TextStyle(fontWeight: FontWeight.bold),
               ),
-              actions: [
-                ConfirmButtons(
-                  onConfirm: () async {
-                    Navigator.pop(context);
-                    final segnalazioneService = SegnalazioneService();
-                    final trenoService = TrenoService();
-                    print("Fase di salvaraggio");
-                    final trenoAsObj = await trenoService.getSingleTrenoByCodice(serial!);
-                    //final trenoAsObj = trenoAsList[0];
-                    final id = segnalazioneService.generateId();
-                    print("Treno da salvare: $trenoAsObj");
-                    
+              const TextSpan(text: " premendo "),
+              TextSpan(
+                text: "QUI",
+                style: const TextStyle(
+                  color: Colors.red,
+                  fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.underline,
+                ),
+                recognizer: TapGestureRecognizer()..onTap = _call112,
+              ),
+              const TextSpan(
+                text:
+                    "\n\nSe vuoi procedere a segnalare l'evento solo al personale di bordo premi il pulsante in basso.\nTi ricordiamo che questa app non si sostituisce in alcun modo al numero unico di emergenza nazionale.\nRicordiamo inoltre che l'attivazione indebita di un ALERT rappresenta un illecito che sarà sanzionato a norma del D.P.R. 753/80",
+              ),
+            ],
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text("Annulla"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+              _showConfirmDialog(context);
+            },
+            child: const Text("Continua segnalazione"),
+          ),
+        ],
+      ),
+    );
+  }
 
-                    final segnalazione = Segnalazione(
-                      idNotifica: id, 
-                      tipo: title, 
-                      treno: trenoAsObj ?? Treno(codice: "FALLBACK_NO_TRAIN_FOUND", nCarrozze: "0", dataOraPartenza: "MAI", dataOraArrivo: "MAI"), 
-                      carrozza: carriage!, 
-                      dataOraApertura: DateTime.now().toString().substring(0, 16), 
-                      stato: 'APERTA', 
-                      apertaDa: currentUser);
-                    segnalazioneService.createSegnalazione(segnalazione);
+  Future<void> _call112() async {
+    final uri = Uri(scheme: 'tel', path: '112');
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    }
+  }
 
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: RichText(
-                          text: TextSpan(
-                            style: const TextStyle(color: Colors.white),
-                            children: [
-                              const TextSpan(
-                                  text:
-                                      "Segnalazione inviata. Per visualizzarla premi "),
-                              TextSpan(
-                                text: "QUI",
-                                style: const TextStyle(
-                                  color: Colors.yellow,
-                                  decoration: TextDecoration.underline,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                                recognizer: TapGestureRecognizer()
-                                  ..onTap = () {
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) =>
-                                            NotificationsPage(
-                                              treno: serial,
-                                              carrozza: carriage,
-                                              tipoSegnalazione: title,
-                                              currentUser: currentUser,
-                                        ),
-                                      ),
-                                    );
-                                  },
-                              ),
-                            ],
-                          ),
+  void _showConfirmDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        title: const Text("Conferma segnalazione"),
+        content: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Stai aprendo una segnalazione ${widget.title} per:",
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 16),
+              // Treno
+              TextFormField(
+                controller: _trenoCtrl,
+                decoration: const InputDecoration(
+                  labelText: "Treno",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 12),
+              // Carrozza
+              TextFormField(
+                controller: _carrozzaCtrl,
+                decoration: const InputDecoration(
+                  labelText: "Carrozza",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 16),
+              // Media
+              OutlinedButton.icon(
+                icon: const Icon(Icons.camera_alt),
+                label: Text(
+                  mediaFile == null ? "Aggiungi foto / video" : "Media selezionato",
+                ),
+                onPressed: _pickMedia,
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                "L'apertura di una segnalazione fasulla è un reato.",
+                style: TextStyle(fontSize: 12, color: Colors.red),
+              ),
+            ],
+          ),
+        ),
+        actions: [
+          /* TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text("Annulla"),
+          ), */
+          ConfirmButtons(
+            onConfirm: () {
+              Navigator.pop(context);
+              _onConfirmSegnalazione(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+
+
+
+  Future<void> _onConfirmSegnalazione(BuildContext context) async {
+  final segnalazioneService = SegnalazioneService();
+  final trenoService = TrenoService();
+
+  final trenoAsObj = await trenoService.getSingleTrenoByCodice(_trenoCtrl.text);
+  final id = segnalazioneService.generateId();
+
+  String? mediaB64;
+  String? mediaFileName;
+
+  if (mediaFile != null) {
+    try {
+      // Usa XFile.readAsBytes() con await
+      final fileBytes = await mediaFile!.readAsBytes();
+      if (fileBytes.isNotEmpty) {
+        mediaB64 = base64Encode(fileBytes);
+        mediaFileName = mediaFile?.name ?? 'allegato_${id}.bin';
+
+      } else {
+        print("File selezionato vuoto");
+      }
+    } catch (e) {
+      print("Errore lettura file: $e");
+    }
+  }
+
+  final segnalazione = Segnalazione(
+    idNotifica: id,
+    tipo: widget.title,
+    treno: trenoAsObj ??
+        Treno(
+          codice: "FALLBACK_NO_TRAIN_FOUND",
+          nCarrozze: "0",
+          dataOraPartenza: "MAI",
+          dataOraArrivo: "MAI",
+        ),
+    carrozza: _carrozzaCtrl.text,
+    dataOraApertura: DateTime.now().toString().substring(0, 16),
+    stato: 'APERTA',
+    apertaDa: widget.currentUser,
+    mediaUrl: mediaB64 ?? "",
+    mediaFileName: mediaFileName ?? ""
+  );
+
+  await segnalazioneService.createSegnalazione(segnalazione);
+  _showSuccessSnackBar(context);
+}
+
+
+  void _showSuccessSnackBar(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: RichText(
+          text: TextSpan(
+            style: const TextStyle(color: Colors.white),
+            children: [
+              const TextSpan(text: "Segnalazione inviata. Per visualizzarla premi "),
+              TextSpan(
+                text: "QUI",
+                style: const TextStyle(
+                  color: Colors.yellow,
+                  decoration: TextDecoration.underline,
+                  fontWeight: FontWeight.bold,
+                ),
+                recognizer: TapGestureRecognizer()
+                  ..onTap = () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => NotificationsPage(
+                          treno: _trenoCtrl.text,
+                          carrozza: _carrozzaCtrl.text,
+                          tipoSegnalazione: widget.title,
+                          currentUser: widget.currentUser,
                         ),
-                        duration: const Duration(seconds: 10),
-                        behavior: SnackBarBehavior.floating,
                       ),
                     );
                   },
-                ),
-              ],
-            ),
-          );
-        },
+              ),
+            ],
+          ),
+        ),
+        duration: const Duration(seconds: 10),
+        behavior: SnackBarBehavior.floating,
       ),
     );
   }
 }
+
+
