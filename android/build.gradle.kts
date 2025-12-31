2da447da8c4f6f508991785123364e48c3da9a1d
Antonio Carraturo <a.carraturo@almaviva.it>
2025-12-31 13:04:58 +0100

    Fix google-service version in build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     // Plugin Google Services (Firebase)
-    id("com.google.gms.google-services") version "4.4.1" apply false
+    id("com.google.gms.google-services") version "4.3.15" apply false
 }
 allprojects {
     repositories {
