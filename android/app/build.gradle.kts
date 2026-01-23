3bba61209b2b382ebf7af716dd0e33f86089aa3a
Angelo-Cordella <angelo.cordella.02@gmail.com>
2026-01-23 14:00:14 +0100

    Risolto problema Play Console (forse)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,7 +19,6 @@ dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
 
-
 android {
     namespace = "com.travelwithsecurity.fsapp"
     compileSdk = flutter.compileSdkVersion
@@ -28,7 +27,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-		isCoreLibraryDesugaringEnabled = true
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -36,41 +35,47 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.travelwithsecurity.fsapp"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
-	signingConfigs {
-		create("release") {
-			keyAlias = keystoreProperties["keyAlias"] as String
-			keyPassword = keystoreProperties["keyPassword"] as String
-			storeFile = file(keystoreProperties["storeFile"] as String)
-			storePassword = keystoreProperties["storePassword"] as String
-		}
-	}
+    signingConfigs {
+        if (keystorePropertiesFile.exists()) {
+            val keyAlias = keystoreProperties.getProperty("keyAlias")
+            val keyPassword = keystoreProperties.getProperty("keyPassword")
+            val storeFilePath = keystoreProperties.getProperty("storeFile")
+            val storePassword = keystoreProperties.getProperty("storePassword")
 
-	//vecchio
-    //buildTypes {
-    //    release {
-    //        // TODO: Add your own signing config for the release build.
-    //        // Signing with the debug keys for now, so `flutter run --release` works.
-    //        //signingConfig = signingConfigs.getByName("debug")
-    //    }
-    //}
-	buildTypes {
-		release {
-			signingConfig = signingConfigs.getByName("release")
-			isMinifyEnabled = false
-			isShrinkResources = false
-		}
-	}
+            val allPresent = !keyAlias.isNullOrBlank()
+                    && !keyPassword.isNullOrBlank()
+                    && !storeFilePath.isNullOrBlank()
+                    && !storePassword.isNullOrBlank()
 
+            if (allPresent) {
+                create("release") {
+                    this.keyAlias = keyAlias
+                    this.keyPassword = keyPassword
+                    this.storeFile = file(storeFilePath!!)
+                    this.storePassword = storePassword
+                }
+            }
+        }
+    }
+
+    buildTypes {
+        release {
+            signingConfig = if (signingConfigs.findByName("release") != null) {
+                signingConfigs.getByName("release")
+            } else {
+                signingConfigs.getByName("debug")
+            }
+            isMinifyEnabled = false
+            isShrinkResources = false
+        }
+    }
 }
 
 flutter {
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,11 @@
+import org.gradle.api.file.Directory
+import org.gradle.api.tasks.Delete
+
 plugins {
-    // Plugin Google Services (Firebase)
+    // Versione UNICA del plugin Firebase / Google Services
     id("com.google.gms.google-services") version "4.3.15" apply false
 }
+
 allprojects {
     repositories {
         google()
@@ -9,20 +13,22 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
+// Centralizza le cartelle build sotto /build
+val newBuildDir: Directory = rootProject.layout.buildDirectory
+    .dir("../../build")
+    .get()
+
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
+
+    // Necessario per Flutter
     project.evaluationDependsOn(":app")
 }
 
+// Task clean globale
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
